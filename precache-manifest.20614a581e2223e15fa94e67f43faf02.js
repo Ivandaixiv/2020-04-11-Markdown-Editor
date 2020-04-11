e018dd59a122690f6b845d5ee3aaf40aa5bf1272
Ivan <ivandaixivwork@gmail.com>
2020-04-11 09:51:22 -0700

    Updates

---
+++ precache-manifest.20614a581e2223e15fa94e67f43faf02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4308d4b6066c66426a1d79a6771999ec",
+    "url": "/Ivandaixiv/2020-04-11-Markdown-Editor/index.html"
+  },
+  {
+    "revision": "a257875bd61a633c0fc4",
+    "url": "/Ivandaixiv/2020-04-11-Markdown-Editor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c2b00753f95ecad76e45",
+    "url": "/Ivandaixiv/2020-04-11-Markdown-Editor/static/js/2.6df27067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ivandaixiv/2020-04-11-Markdown-Editor/static/js/2.6df27067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a257875bd61a633c0fc4",
+    "url": "/Ivandaixiv/2020-04-11-Markdown-Editor/static/js/main.d37bb972.chunk.js"
+  },
+  {
+    "revision": "f9fea6114b6d718515fa",
+    "url": "/Ivandaixiv/2020-04-11-Markdown-Editor/static/js/runtime-main.7c244322.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ivandaixiv/2020-04-11-Markdown-Editor/static/media/logo.5d5d9eef.svg"
+  }
+]);
